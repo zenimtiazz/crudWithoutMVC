85c452a7747de89208f144497e57d7858cc94593
zain Imtiaz <zain@zains-MacBook-Air.local>
2021-12-08 09:22:16 +0100

    modified

--- dump-crud-202112080854.sql
+++ dump-crud-202112080854.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE `crud` ;
 
 CREATE TABLE `CLASS` (
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `location` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
   `c_id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`c_id`)
 ) ;
@@ -11,8 +11,8 @@ INSERT INTO `CLASS` VALUES ('Lammar','Antwerp',1),('Giertz','Gent',2),('Giertz',
 
 CREATE TABLE `STUDENT` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `email` varchar(100)  DEFAULT NULL,
   `c_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `STUDENT_FK` (`c_id`),
@@ -22,8 +22,8 @@ CREATE TABLE `STUDENT` (
 INSERT INTO `STUDENT` VALUES (1,'zain','zenimtiaz@gmail.com',1),(2,'Daryle','darylbecode@gmail.com',1),(3,'sofiane','sofianebecode@gmail.com',1),(4,'neha','nehabecode@gmail.com',1),(5,'nama','namabecode@gmail.com',2),(7,'iara','iarakhan@gmail.com',3),(9,'sonia','sonia@gmail.com',3),(12,'xioqi','xiaoqiki@gmail.com',2),(13,'ali','ali@gmail.com',3);
 
 CREATE TABLE `TEACHER` (
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `email` varchar(100)  DEFAULT NULL,
   `id` int DEFAULT NULL,
   `c_id` int DEFAULT NULL,
   KEY `TEACHER_FK_1` (`c_id`),
